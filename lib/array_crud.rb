e159fc7965237a35de1b6bebe2ca795ec7a6dbbd
Abraham Moreno <adiaz2821@gmail.com>
2019-09-16 14:14:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   ["Joel", "Abraham", "Martin"]
-  Martin = 
+  Martin = remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
