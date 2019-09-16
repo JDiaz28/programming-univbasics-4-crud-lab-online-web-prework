d655f9e0b3ad0d20d4c081a08c7d655df2c951f1
Abraham Moreno <adiaz2821@gmail.com>
2019-09-16 14:20:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Joel", "Abraham", "Martin"]
-  << "Mago"
+  "Mago"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -33,12 +33,12 @@ end
 
 def retrieve_first_element_from_array(array)
   ["Joel", "Abraham", "Martin"]
-  retrieve_first_element_from_array[0]
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
   ["Joel", "Abraham", "Martin"]
-  retrieve_last_element_from_array[-1]
+  [-1]
 end
 
 def update_element_from_index(array, index_number, element)
