3e128aef6ef8efc9574cc9cc7e8375f8fbd33aca
Abraham Moreno <adiaz2821@gmail.com>
2019-09-16 14:09:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [5, 6, 7]
-  << 8
+  ["Juan", "Martin", "Joel"]
+  << "Mago"
 end
 
 def add_element_to_start_of_array(array, element)
